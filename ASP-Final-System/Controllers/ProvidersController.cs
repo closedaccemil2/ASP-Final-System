26c379d144173ccdb43e519d28ee497f1faeaa57
Emil Barranco <rehser@outlook.com>
2020-03-29 11:14:44 -0400

    Creación del Proyecto
    - Modelos de Datos Entity creados.
    - Base de Datos creada y conectada.
    - Cambio de Direccion (RouteConfig) a Products/Index.
    - Creacion de los controladores: Products, Providers, Clients.

---
+++ ASP-Final-System/Controllers/ProvidersController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ASP_Final_System.Models;
+
+namespace ASP_Final_System.Controllers
+{
+    public class ProvidersController : Controller
+    {
+        private SystemModelContainer db = new SystemModelContainer();
+
+        // GET: Providers
+        public ActionResult Index()
+        {
+            return View(db.Providers.ToList());
+        }
+
+        // GET: Providers/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Providers providers = db.Providers.Find(id);
+            if (providers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(providers);
+        }
+
+        // GET: Providers/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Providers/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,RNC,Name,Phone,Email")] Providers providers)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Providers.Add(providers);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(providers);
+        }
+
+        // GET: Providers/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Providers providers = db.Providers.Find(id);
+            if (providers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(providers);
+        }
+
+        // POST: Providers/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,RNC,Name,Phone,Email")] Providers providers)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(providers).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(providers);
+        }
+
+        // GET: Providers/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Providers providers = db.Providers.Find(id);
+            if (providers == null)
+            {
+                return HttpNotFound();
+            }
+            return View(providers);
+        }
+
+        // POST: Providers/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Providers providers = db.Providers.Find(id);
+            db.Providers.Remove(providers);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
